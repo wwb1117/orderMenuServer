1d8c40cff3e7677c788207cb8c52828d654cc2ae
wwb <153634555@qq.com>
2018-09-07 09:39:24 +0800

    wwb

--- router/index.js
+++ router/index.js
@@ -7,13 +7,7 @@ export default app => {
 
 /*----------------------admin-------------------------------*/
     // 用户请求
-    router.post('/api/user/login', user.login)
-    router.get('/api/user/info', user.info)
-    router.get('/api/user/list', user.list)
-    router.post('/api/user/add', user.add)
-    router.post('/api/user/update', user.update)
-    router.get('/api/user/del', user.del)
-
+    router.get('/api/user/login', user.login)
 
     app.use(router.routes()).use(router.allowedMethods());
 }
