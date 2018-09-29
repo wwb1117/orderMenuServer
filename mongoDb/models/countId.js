1e8cbb89e12c964cd89374f0ae7a79ed65feb3d4
wwb <153634555@qq.com>
2018-09-29 18:31:11 +0800

    wwb

--- mongoDb/models/countId.js
+++ mongoDb/models/countId.js
@@ -1,6 +1,6 @@
 import db from '../../mongoDb'
 let countIdSchema = db.Schema({
     fieldName: String,
-	fieldId: Number
+	fieldId: {type: Number, default: 0}
 })
 export default db.model('countId', countIdSchema);
