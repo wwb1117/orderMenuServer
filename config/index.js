5df21fd08fa0362d76b3f04c9fc472f6d387a51d
wwb <153634555@qq.com>
2018-11-28 10:47:49 +0800

    wwb

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const log = {
     projectName: 'menu', // 项目名，记录在日志中的项目信息
     ip: '0.0.0.0' // 默认情况下服务器 ip 地址
 }
-const port = '3000'
+const port = '3040'
 
 export default {
     env: process.env.NODE_ENV,
